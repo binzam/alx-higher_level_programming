507229f59acea753b17496242ccec08670de9cbf
binyam <btechan@gmail.com>
2023-09-07 17:09:10 -0700

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+list_of_args = sys.argv[1:]
+
+int_args = [int(arg) for arg in list_of_args]
+
+sum_of_args = 0
+for num in int_args:
+    sum_of_args += num
+
+print(sum_of_args)
+
