b70ed58ce8f28b35631c7a5eaa6c25df20a54b0c
binyam <btechan@gmail.com>
2023-09-26 16:55:37 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: length of a side of a quare.
+        """
+        self.__size = size
