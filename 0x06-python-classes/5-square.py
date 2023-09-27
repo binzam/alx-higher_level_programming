dc0e8fda85f9dcb2fbbb61267064c38a076b4077
binyam <btechan@gmail.com>
2023-09-26 17:53:49 -0700

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -42,6 +42,7 @@ class Square:
     def my_print(self):
         """Print this square with the # character."""
         for i in range(0, self.__size):
-            for j in range(self.__size):
-                print("#", end="\n" if j is self.size - 1 and i != j else "")
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
             print("")
