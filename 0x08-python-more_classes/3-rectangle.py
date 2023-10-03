f16781ac9a63c568eb53e584bb8e47aa59a62807
binyam <btechan@gmail.com>
2023-10-02 17:25:33 -0700

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -56,7 +56,7 @@ class Rectangle:
     def __str__(self):
         """Returns the printable string representation of the Rectangle."""
         string = ""
-        if self.__width != 0 or self.__height != 0:
+        if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
                                 for i in range(self.__height))
         return string
