342fd038ab24e5393020949af7b97482977014cc
binyam <btechan@gmail.com>
2023-10-10 17:33:36 -0700

    error t7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,14 @@
 """Script that adds all arguments to a Python list and save them to a file."""
 import sys
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = \
+        __import__("6-load_from_json_file").load_from_json_file
 
-argList = list(sys.argv[1:])
-
-try:
-    data = load_from_json_file("add_item.json")
-except Exception:
-    data = []
-
-data.extend(argList)
-save_to_json_file(data, "add_item.json")
+    try:
+        data = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        data = []
+    data.extend(sys.argv[1:])
+    save_to_json_file(data, "add_item.json")
