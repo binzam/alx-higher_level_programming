c176a982a92e9ad70c9e3f0f1ae0dcf7f8d27236
binyam <btechan@gmail.com>
2023-10-10 18:26:13 -0700

    error fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,13 @@ and then save them to a file
 """
 import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-    load_from_json_file =
-    __import__("8-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+argList = list(sys.argv[1:])
+try:
+    existing_list = load_from_json_file("add_item.json")
+except Exception:
+    existing_list = []
+existing_list.extend(argList)
+save_to_json_file(existing_list, "add_item.json")
