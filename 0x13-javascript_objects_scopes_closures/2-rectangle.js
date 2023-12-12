2bf9ba67ef9b8a325313a8d129b9155ff729b947
binyam <btechan@gmail.com>
2023-12-12 15:13:00 -0800

    task 012

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       return {};
     }
-
     this.width = w;
     this.height = h;
   }
