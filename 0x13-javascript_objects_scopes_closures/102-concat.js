078cd9a4fb2af311b9474a90a6c8e8d6e0ed1184
binyam <btechan@gmail.com>
2023-12-12 16:41:50 -0800

    task advanced

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 const fileA = fs.readFileSync(process.argv[2]).toString();
 const fileB = fs.readFileSync(process.argv[3]).toString();
