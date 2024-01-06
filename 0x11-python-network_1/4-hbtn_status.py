786d19f1c1dc28710fd85105bf2b33c11da6672f
root <btechan@gmail.com>
2024-01-06 15:26:18 -0800

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status
+using the requests package"""
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+    body = response.text
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
