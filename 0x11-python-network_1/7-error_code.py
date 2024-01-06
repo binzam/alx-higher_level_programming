39f9b97abd27f088ce956bc187cc94719b19bd28
root <btechan@gmail.com>
2024-01-06 15:58:28 -0800

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.post(url)
+    response = requests.get(url)
     body = response.text
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
