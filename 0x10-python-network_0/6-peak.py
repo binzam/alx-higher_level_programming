d9258e09d2bc175d28d990032462a9c73d397e01
root <btechan@gmail.com>
2024-01-05 16:15:59 -0800

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,13 +8,13 @@ def find_peak(list_of_integers):
         return None
     left = 0
     right = len(list_of_integers) - 1
-    int_list = list_of_integers
+    list = list_of_integers
     while left < right:
         mid = (left + right) // 2
-        if int_list[mid] > int_list[mid - 1] & int_list[mid] > int_list[mid + 1]:
-            return int_list[mid]
-        elif int_list[mid] < int_list[mid + 1]:
+        if list[mid] > list[mid - 1] & list[mid] > list[mid + 1]:
+            return list[mid]
+        elif list[mid] < list[mid + 1]:
             left = mid + 1
         else:
             right = mid
-    return int_list[left]
+    return list[left]
