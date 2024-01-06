e729c07cf7d28a6229f6f8955584a92d01ab0f4c
root <btechan@gmail.com>
2024-01-06 12:44:12 -0800

    task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message in the body of the response.
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -sLX PUT 0.0.0.0:5000/catch_me_2 -d "user_id=98" -H "Origin: School"
