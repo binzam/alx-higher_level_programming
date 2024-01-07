c5ba47ecd1f7f8c37e50bd659054177db827bea1
root <btechan@gmail.com>
2024-01-07 13:31:35 -0800

    task 4 error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ using the requests package"""
 import requests
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     body = response.text
     print("Body response:")
